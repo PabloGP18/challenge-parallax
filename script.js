46ecfc574dc4b21b2075aba29001259c662bf280
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-26 11:01:23 +0200

    added all the background elements

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // get our character
-let character = document.getElementById("idle")
+let character = document.getElementById("idle");
+
+//get our backgrounds
+let ground = document.getElementById("ground");
+let treesAndBushes = document.getElementById("trees_and_bushes");
+let distantTrees = document.getElementById("distantTrees");
+let hill1 = document.getElementById("hill1");
+let hill2 = document.getElementById("hill2");
+let hugeClouds = document.getElementById("huge_clouds");
+let clouds = document.getElementById("clouds");
+let distantClouds = document.getElementById("distant_clouds");
+
 
 // jump function
 function jumping() {
-    character.classList.add("animate-jump")
-    character.style.backgroundImage = "url(images/jump.gif)"
+    character.classList.add("animate-jump");
+    character.style.backgroundImage = "url(images/jump.gif)";
     setTimeout(function (){
-        character.classList.remove("animate-jump")
-        character.style.backgroundImage = "url(images/run.gif)"
-    }, 800)
-}
+        character.classList.remove("animate-jump");
+        character.style.backgroundImage = "url(images/run.gif)";
+    }, 800);
+};
 
 window.addEventListener("keydown", function(event) {
     jumping();
@@ -17,4 +28,6 @@ window.addEventListener("keydown", function(event) {
         return; // Do nothing if event already handled
     }
 });
+
+var overlap = overlap
 //document.addEventListener('click', jumping);
