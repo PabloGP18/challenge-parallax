0993b840618b6f323b416108b79502455578bc1c
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-27 23:29:21 +0200

    refactored the counter

--- script.js
+++ script.js
@@ -86,25 +86,18 @@ function jumping() {
 };
 
 setInterval(function(){
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
-    if(blockLeft <50 && blockLeft >0 && characterTop >= 550){
-
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<50 && blockLeft>-0 && characterTop>=550){
+        block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
-        counter = 0;
-        block.style.animation = "block 3s infinite linear";
-        character.classList.add("animate-dead");
-        character.style.backgroundImage = "url(images/dead.gif)";
-        setTimeout(function (){
-            character.classList.remove("animate-dead");
-            character.style.backgroundImage = "url(images/idle.gif)";
-        }, 800);
-    }else {
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
-
-},10);
+}, 10);
 
 
 
